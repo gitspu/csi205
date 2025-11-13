746e78323e3bdf688a616c6c47acb4f27283e742
gitspu <natthapol.pho@spumail.net>
2025-11-13 23:12:19 +0700

    delete source code

--- src/components/AppHeader.jsx
+++
@@ -1,9 +0,0 @@
-const AppHeader = () => {
-    return (
-        <>
-            <h1 className="text-center p-5">CSI205 การพัฒนาโปรแกรมส่วนหน้า</h1>
-        </>
-    );
-}
-
-export default AppHeader;
